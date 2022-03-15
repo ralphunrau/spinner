ba04209b828a343b6db48a854af5c109c66eaa1b
Ralph Unrau <xralphunrau@gmail.com>
2022-03-15 20:51:53 +0000

    one loop of spinner!

---
+++ spinner1.js
@@ -0,0 +1,33 @@
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100)
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 200)
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 300)
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 400)
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 500)
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 600)
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 700)
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+  console.log()
+}, 800)
+
